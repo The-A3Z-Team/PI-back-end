47d085d931a73c6472187a6dba893f93a4f8e8e0
Achraf TAFFAH <a.taffah@etu.enset-media.ac.ma>
2023-05-16 21:44:42 +0200

    negociation-service

---
+++ Negociation-service/src/main/java/org/sid/negociationservice/NegociationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sid.negociationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegociationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegociationServiceApplication.class, args);
+	}
+
+}
---
+++ Negociation-service/src/test/java/org/sid/negociationservice/NegociationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.negociationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegociationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
