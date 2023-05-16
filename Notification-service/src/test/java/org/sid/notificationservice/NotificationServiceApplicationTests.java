1ade9a6abd556d2332fb297383b0c81fb17c0e92
Achraf TAFFAH <a.taffah@etu.enset-media.ac.ma>
2023-05-16 21:51:37 +0200

    notification-service

---
+++ Notification-service/src/test/java/org/sid/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
